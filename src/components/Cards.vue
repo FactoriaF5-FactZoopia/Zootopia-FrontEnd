deed02557c550aa087ac4cdd41ee2086d0dcd85f
Oscar <oscar15-91@hotmail.com>
2024-08-07 14:20:04 +0200

    añadido views

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,29 +1,28 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" id="btn" class="btn btn-primary" >EDIT</a>
-    <a href="#" id="btn" class="btn btn-danger" >DELETE</a>
-
-  </div>
-</div>
-
+  <main>
+    <div class="card" style="width: 18rem">
+      <img class="card-img-top" src="..." alt="Card image cap" />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" id="btn" class="btn btn-primary">EDIT</a>
+        <a href="#" id="btn" class="btn btn-danger">DELETE</a>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-
 #card-title {
-    text-align: center;
+  text-align: center;
 }
 
 #btn {
-    margin-left: 20px;
+  margin-left: 20px;
 }
-
 </style>
