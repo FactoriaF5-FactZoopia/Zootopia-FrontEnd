cd7711145b7753f9fa7e8153a072160e2489d4c6
eyadcode24 <eyadcode24@gmail.com>
2024-08-07 14:31:57 +0200

    cards medio hecho

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -3,20 +3,83 @@
 </script>
 
 <template>
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" id="btn" class="btn btn-primary" >EDIT</a>
-    <a href="#" id="btn" class="btn btn-danger" >DELETE</a>
+  <div id = "container">
+
+  
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="../assets/felino1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Felids</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div id = "botton">
+        <a href="#" id="btn" class="btn btn-primary" ><img class="bin" src="../assets/edit.png" alt="Card image cap"> </a>
+        <a href="#" id="btn" class="btn btn-danger" >   <img class="bin" src="../assets/bin.png" alt="Card image cap"> </a>
+    </div>
+    </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="../assets/Canids.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Canids</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div id = "botton">
+        <a href="#" id="btn" class="btn btn-primary" ><img class="bin" src="../assets/edit.png" alt="Card image cap"> </a>
+        <a href="#" id="btn" class="btn btn-danger" >   <img class="bin" src="../assets/bin.png" alt="Card image cap"> </a>
+    </div>
+    </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="../assets/Reptiles.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Reptiles</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div id = "botton">
+        <a href="#" id="btn" class="btn btn-primary" ><img class="bin" src="../assets/edit.png" alt="Card image cap"> </a>
+        <a href="#" id="btn" class="btn btn-danger" >   <img class="bin" src="../assets/bin.png" alt="Card image cap"> </a>
+    </div>
+    </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="../assets/Mustelids.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Mustelids</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div id = "botton">
+        <a href="#" id="btn" class="btn btn-primary" ><img class="bin" src="../assets/edit.png" alt="Card image cap"> </a>
+        <a href="#" id="btn" class="btn btn-danger" >   <img class="bin" src="../assets/bin.png" alt="Card image cap"> </a>
+    </div>
+    </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="../assets/Leporidae.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Leporidae</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div id = "botton">
+        <a href="#" id="btn" class="btn btn-primary" ><img class="bin" src="../assets/edit.png" alt="Card image cap"> </a>
+        <a href="#" id="btn" class="btn btn-danger" >   <img class="bin" src="../assets/bin.png" alt="Card image cap"> </a>
+    </div>
+    </div>
+    </div>
+
 
   </div>
-</div>
 
 </template>
 
 <style scoped>
+#container{
+  display: flex;
+
+}
+.bin{
+  width: 25px;
+
+}
 
 #card-title {
     text-align: center;
@@ -25,5 +88,13 @@
 #btn {
     margin-left: 20px;
 }
+#bin{
+  width: 1px;
+  height: 1px;
+}
+#card-img-top{
+  width: 50px;
+  height: 70px;
 
+}
 </style>
