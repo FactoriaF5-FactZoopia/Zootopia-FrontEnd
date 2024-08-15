0311f3f9a21e0544518385443c9878a67a1e4691
Oscar <oscar15-91@hotmail.com>
2024-08-15 20:52:06 +0200

    añadidas cartas de animales

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,31 @@ const router = createRouter({
       component: () => import("../views/AnimalsView.vue"),
     },
     {
+      path: "/felids",
+      name: "felids",
+      component: () => import("../views/FelidosView.vue"),
+    },
+    {
+      path: "/canids",
+      name: "canids",
+      component: () => import("../views/CanidosView.vue"),
+    },
+    {
+      path: "/reptils",
+      name: "reptils",
+      component: () => import("../views/ReptilesView.vue"),
+    },
+    {
+      path: "/mustelids",
+      name: "mustelids",
+      component: () => import("../views/MustelidosView.vue"),
+    },
+    {
+      path: "/leporidae",
+      name: "leporidae",
+      component: () => import("../views/LeporidaeView.vue"),
+    },
+    {
       path: "/animalsFilter",
       name: "animalsFilter",
       component: () => import("../views/AnimalsFilter.vue"),
